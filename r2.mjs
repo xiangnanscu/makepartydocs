114a8e18878101b7259c673d5dd8ed9fed90854f
xiangnan <280145668@qq.com>
2024-10-14 12:02:40 +0800

    done

--- r2.mjs
+++ r2.mjs
@@ -26,6 +26,7 @@ const s3Client = new S3Client({
     })
   }
 });
+
 const extentios = {
   '.txt': 'text/plain',
   '.mp4': 'video/mp4',
@@ -38,13 +39,21 @@ const extentios = {
   '.pdf': 'application/pdf',
   '.doc': 'application/msword',
 }
+
 const outputDir = './output';
 // 记录映射关系
-const nameMapPath = path.join(outputDir, 'name.json');
-let nameMap = {};
-if (fs.existsSync(nameMapPath)) {
-  nameMap = JSON.parse(await fs.promises.readFile(nameMapPath, 'utf-8'));
+const successFiles = path.join(outputDir, 'success.json');
+const errorFiles = path.join(outputDir, 'error.json');
+let successMap = {};
+if (fs.existsSync(successFiles)) {
+  successMap = JSON.parse(await fs.promises.readFile(successFiles, 'utf-8'));
 }
+let errorMap = {};
+if (fs.existsSync(errorFiles)) {
+  errorMap = JSON.parse(await fs.promises.readFile(errorFiles, 'utf-8'));
+}
+let ok = 0
+let fail = 0
 // 递归遍历文件夹并上传文件
 async function uploadFiles(dir) {
   const files = await fs.promises.readdir(dir);
@@ -67,17 +76,34 @@ async function uploadFiles(dir) {
         client: s3Client,
         params,
       });
+      if (successMap[file]) {
+        console.log(`文件已存在,跳过: ${file}`)
+        continue
+      }
+      console.log(`开始上传: ${file}`);
 
-      await upload.done();
-      console.log(`${filePath} => ${fileName}`);
-      nameMap[file] = `https://r2.xnscu.com/${uuidName}${ext}`;
-      await fs.promises.writeFile(nameMapPath, JSON.stringify(nameMap, null, 2));
+      upload.on('httpUploadProgress', (progress) => {
+        const percentComplete = Math.round((progress.loaded / progress.total) * 100);
+        console.log(`${file} 上传进度: ${percentComplete}%`);
+      });
+
+      try {
+        await upload.done();
+        ok++
+        console.log(`${file} 上传完成: ${filePath} => ${fileName}`);
+        successMap[file] = `https://r2.xnscu.com/${fileName}`;
+        await fs.promises.writeFile(successFiles, JSON.stringify(successMap, null, 2));
+      } catch (error) {
+        fail++
+        console.error(`上传 ${file} 时发生错误:`, error.message);
+        errorMap[file] = error.message
+        await fs.promises.writeFile(errorFiles, JSON.stringify(errorMap, null, 2));
+      }
     }
   }
 }
 
 // 启动上传
-
 uploadFiles(outputDir)
-  .then(() => console.log('All files uploaded successfully'))
-  .catch(err => console.error('Error uploading files:', err));
+  .then(() => console.log(`所有文件处理完毕,成功:${ok}, 失败:${fail}`))
+  .catch(err => console.error('上传文件时出错:', err));
